bbe2c51a8630944b768a8f5379cd5342ae3460f8
BotRamenRobotics <redstonegamer360@gmail.com>
2023-01-04 19:29:27 -0500

    creating elevator subsystem framework (very much wip)

---
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Elevator extends SubsystemBase{
+    
+    private final TalonFX BaseMotorMaster, BaseMotorFollower;
+    
+}
